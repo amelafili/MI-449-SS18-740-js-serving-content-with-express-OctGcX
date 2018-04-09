0e0b104682ed352b1ea8b5972b61d542fb4dcad0
amelafili <31477792+amelafili@users.noreply.github.com>
2018-04-09 19:03:56 -0400

    edited index and server

--- server.js
+++ server.js
@@ -2,17 +2,8 @@ var express = require('express')
 var app = express()
 var port = process.env.PORT || 8080
 
-var article = {
-  title: 'Why Turtles are the Best Animals Ever',
-  content: 'This page is dedicated to informing you all to my non-biased opinion on why' +
-  'turtles are the most superior animal'
-}
-app.get('/', function (request, response) {
-  response.render('pages/index', {
-    articles: article
-  })
-})
-
 app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
 
 app.listen(port)
